cd99fdb5a856adaf3bd853549b7d645587a22b62
Seokhwan Jung <48024026+ejungsh96@users.noreply.github.com>
2022-04-04 16:25:42 +0200

    LeetCode 28

--- LeetCode Problems.py
+++ LeetCode Problems.py
@@ -2077,3 +2077,36 @@ class Solution:
     def squared_distance(self, point: List[int]) -> int:
         """Calculate and return the squared Euclidean distance."""
         return point[0] ** 2 + point[1] ** 2
+
+                                                                                             28. Implement strStr()
+Implement strStr().
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+Clarification:
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+Example 1:
+Input: haystack = "hello", needle = "ll"
+Output: 2
+ 
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        l = len(needle)
+        window = deque(haystack[:l])
+        
+        print(window)
+        
+        if list(window) == list(needle):
+            return 0
+        
+        for i in range(0, len(haystack) - l):
+            if list(window) == list(needle):
+                return i
+            else:
+                window.popleft()
+                window.append(haystack[i + l])
+                print(f"appended: {window}")
+        
+        if list(window) == list(needle):
+            return len(haystack) - l
+        
+        return -1
