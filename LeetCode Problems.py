759b00d6fd7f54386a97ad9825ed5b21216628ba
Seokhwan Jung <48024026+ejungsh96@users.noreply.github.com>
2022-06-08 00:52:36 +0100

    LeetCode 74

--- LeetCode Problems.py
+++ LeetCode Problems.py
@@ -2368,3 +2368,94 @@ class Solution:
             max_count = max(max_count, right - left + 1)
         
         return max_count
+
+74. Search a 2D Matrix
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+Example 1:
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+ 
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        low = 0
+        high = len(matrix) - 1
+        
+        # if the input is just one element
+        if len(matrix) == len(matrix[0]) == 1:
+            if matrix[0][0] == target:
+                return True
+            else:
+                return False
+        
+        # if the input is 1D array
+        if low == high:
+            return self.binary_search_row(matrix[0], target)
+        
+        # check if the target is at the last row
+        if target >= matrix[high][0]:
+            return self.binary_search_row(matrix[high], target)
+                
+        while low <= high:
+            mid = (low + high) // 2
+            
+            if matrix[mid][0] <= target <= matrix[mid][-1]:
+                return self.binary_search_row(matrix[mid], target)
+            elif target < matrix[mid][0]:
+                high = mid - 1
+            elif target > matrix[mid][0]:
+                low = mid + 1
+        
+        if not (high <= low):
+            return False
+        
+        mid = (low + high) // 2
+ 
+        return self.binary_search_row(matrix[mid], target)
+        
+    
+    def binary_search_row(self, row, target):
+        left = 0
+        right = len(row) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if target == row[mid]:
+                return True
+            elif target < row[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return False
+    
+# class Solution:
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         ROWS, COLS = len(matrix), len(matrix[0])
+        
+#         top, bot = 0, ROWS - 1
+#         while top <= bot:
+#             row = (top + bot) // 2
+#             if target > matrix[row][-1]:
+#                 top = row + 1
+#             elif target < matrix[row][0]:
+#                 bot = row - 1
+#             else:
+#                 break
+        
+#         if not (top <= bot):
+#             return False
+#         row = (top + bot) // 2
+#         l, r = 0, COLS - 1
+#         while l <= r:
+#             m = (l + r) // 2
+#             if target > matrix[row][m]:
+#                 l = m + 1
+#             elif target < matrix[row][m]:
+#                 r = m - 1
+#             else:
+#                 return True
+#         return False
