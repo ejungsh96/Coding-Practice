cf654cd9091197c60ec7ac76c8f488e91685ad3e
Seokhwan Jung <48024026+ejungsh96@users.noreply.github.com>
2022-06-09 17:24:04 +0100

    LeetCode 213

--- LeetCode Problems.py
+++ LeetCode Problems.py
@@ -2533,3 +2533,27 @@ class Solution:
             else:
                 l = m + 1
         return k
+
+213. House Robber II
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if two adjacent houses were broken into on the same night.
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+Example 1:
+Input: nums = [2,3,2]
+Output: 3
+Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) <= 3:
+            return max(nums)
+        
+        def rob_line(nums: List[int]) -> int:
+            rob1, rob2 = 0, 0
+ 
+            # [rob1, rob2, n, n+1, ...]
+            for n in nums:
+                temp = max(n + rob1, rob2)
+                rob1 = rob2
+                rob2 = temp
+            return rob2
+        
+        return max(rob_line(nums[:-1]), rob_line(nums[1:]))
